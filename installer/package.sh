458df7c47c37740be878f22bde95b439d468b8fd
Jason Champion <jchampion@zetacentauri.com>
2016-10-10 15:00:23 -0700

    Add script to code sign binaries and create a signed .pkg file.

---
+++ installer/package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Frameworks/libportaudio.2.dylib --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
+codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Frameworks/librtmidi.2.1.0.dylib --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
+codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Frameworks/libSDL2-2.0.0.dylib --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
+codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Frameworks/libSDL2_mixer-2.0.0.dylib --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
+codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
+#codesign -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Resources/Entitlements.plist
+#productbuild --sign "Developer ID Installer: Jason Champion (G6PFF85X2R)" --component DrumPads.app /Applications DrumPads.pkg
+productbuild --sign "3rd Party Mac Developer Installer: Jason Champion (G6PFF85X2R)" --component DrumPads.app /Applications DrumPads.pkg
