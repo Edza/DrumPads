41134aed248767c61bcf663817d70f7dfe71466b
Jason Champion <jchampion@zetacentauri.com>
2016-10-11 13:34:31 -0700

    Add sample copy to package command for osx.

--- installer/package.sh
+++ installer/package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp -R ../samples_v2/*.wav DrumPads.app/Contents/Resources/Samples/
 codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Frameworks/libportaudio.2.dylib --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
 codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Frameworks/librtmidi.2.1.0.dylib --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
 codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" DrumPads.app/Contents/Frameworks/libSDL2-2.0.0.dylib --entitlements DrumPads.app/Contents/Resources/Entitlements.plist
